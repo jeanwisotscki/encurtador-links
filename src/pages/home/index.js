be88d83978aa15c62a6e7f44bf83599817ebeb8f
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-17 14:58:34 -0300

    routes feito

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="container-home">
+      <h1>Página Home</h1>
+      <div className="logo">
+        <img src="./logo.png" alt="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
