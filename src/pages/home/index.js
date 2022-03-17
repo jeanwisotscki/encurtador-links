af6993eae0a1510ee9688cd6a9b1305810e0cbf2
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-17 17:33:28 -0300

    pagina home ok

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
+import { FiLink } from "react-icons/fi";
+import "./home.css";
+
 function Home() {
   return (
     <div className="container-home">
-      <h1>Página Home</h1>
       <div className="logo">
-        <img src="./logo.png" alt="logo" />
+        <FiLink size={200} color="#fff" />
+
+        <h1>EncurtaLink</h1>
+        <span>Cole seu link para encurta-lo 👇</span>
+      </div>
+
+      <div className="area-input">
+        <div className="icon">
+          <FiLink size={24} color="#fff" />
+          <input type="text" placeholder="Cole seu link aqui" />
+        </div>
+
+        <button>Encurtar link</button>
       </div>
     </div>
   );
