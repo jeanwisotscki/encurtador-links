53ff7201f4b0a796292cd3b026f57238a3b1dd6a
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-18 09:00:41 -0300

    pagina meus links ok

--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -1,7 +1,26 @@
+import "./links.css";
+import { FiArrowLeft, FiLink, FiTrash } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 function Links() {
   return (
-    <div>
-      <h1>Página Links</h1>
+    <div className="links-container">
+      <div className="links-header">
+        <Link to="/">
+          <FiArrowLeft size={38} color="#fff" />
+        </Link>
+        <h1>Meus Links</h1>
+      </div>
+
+      <div className="links-item">
+        <button className="link">
+          <FiLink size={18} color="#fff" />
+          https://www.google.com.br
+        </button>
+        <button className="link-delete">
+          <FiTrash size={24} color="tomato" />
+        </button>
+      </div>
     </div>
   );
 }
