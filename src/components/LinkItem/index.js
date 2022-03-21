47fd85954e66b86f65caea52bc6d11f251a234c2
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-21 19:07:15 -0300

    funcionalidades home ok

--- src/components/LinkItem/index.js
+++ src/components/LinkItem/index.js
@@ -1,7 +1,12 @@
 import "./link-item.css";
 import { FiX, FiClipboard } from "react-icons/fi";
 
-export default function LinkItem({ closeModal }) {
+export default function LinkItem({ closeModal, content }) {
+  async function copyLink() {
+    await navigator.clipboard.writeText(content.link);
+    alert("Link copiado com sucesso!");
+  }
+
   return (
     <div className="modal-container">
       <div className="modal-header">
@@ -11,10 +16,10 @@ export default function LinkItem({ closeModal }) {
         </button>
       </div>
 
-      <span>https://github.com/jeanwisotscki</span>
+      <span> {content.long_url} </span>
 
-      <button className="modal-link">
-        https://github.com/jeanwisotscki
+      <button className="modal-link" onClick={copyLink}>
+        {content.link}
         <FiClipboard size={28} color="#fff" />
       </button>
     </div>
